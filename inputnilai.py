7997a522fee15114f31bff0cae2b130c3fd0da60
Enengsalwa <160403792+Enengsalwa@users.noreply.github.com>
2024-03-04 07:09:25 +0700

    Add files via upload

---
+++ inputnilai.py
@@ -0,0 +1,5 @@
+nilai = int(input("masukan nilai anda :"))
+if nilai > 75:
+    print("selamat anda lulus")
+else:
+    print("maaf anda tidak lulus")
---
+++ konversinilai.py
@@ -0,0 +1,14 @@
+nilai = int (input("masukan nilai anda :"))
+
+if nilai <= 50:
+    print("nilai anda E")
+elif nilai >= 50 and nilai <=60:
+    print("nilai anda D")
+elif nilai >= 60 and nilai <=70:
+    print("nilai anda C")
+elif nilai >= 70 and nilai <=80:
+    print("nilai anda B")
+elif nilai >= 80 and nilai <=100:
+    print("nilai anda A")
+else:
+    print("nilai tidak valid")
